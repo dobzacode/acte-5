c9e60cd0d5f1c5cf6c8cf113624988ce67768303
Corentin Kittel <118741789+dobzacode@users.noreply.github.com>
2024-04-30 09:44:53 +0200

    fix semantic on section links in nav

--- components/ui/header/section-links.tsx
+++ components/ui/header/section-links.tsx
@@ -6,9 +6,8 @@ export default function SectionLinks() {
   const pathname = usePathname();
 
   return (
-    <>
-      <Link
-        href="/evenement"
+    <ul className="flex h-full w-full">
+      <li
         className={cn(
           'relative flex w-[120px] origin-center justify-center overflow-hidden rounded-l-xs  text-default-400 duration-medium before:absolute before:-z-10 before:h-full before:w-full before:translate-x-full before:rounded-xs before:bg-gradient-to-t before:from-primary-200  before:to-primary-50  ',
           pathname.includes('evenement') && 'text-black before:translate-x-0',
@@ -17,10 +16,9 @@ export default function SectionLinks() {
             : ''
         )}
       >
-        Evenementiel
-      </Link>
-      <Link
-        href="/spectacle"
+        <Link href="/evenement">Evenementiel</Link>
+      </li>
+      <li
         className={cn(
           'relative flex w-[90px] origin-center justify-center overflow-hidden rounded-r-xs text-default-400 duration-medium before:absolute before:-z-10 before:h-full before:w-full before:-translate-x-full before:rounded-xs before:bg-gradient-to-t before:from-primary-200 before:to-primary-50  hover:text-black',
           pathname.includes('spectacle') && 'text-black before:translate-x-0',
@@ -29,8 +27,8 @@ export default function SectionLinks() {
             : ''
         )}
       >
-        Spectacle
-      </Link>
-    </>
+        <Link href="/spectacle">Spectacle</Link>
+      </li>
+    </ul>
   );
 }
