fb26f9c5b548c1d0ed37db9573bccbcea74c432b
Corentin Kittel <118741789+dobzacode@users.noreply.github.com>
2024-05-04 22:21:07 +0200

    fix import

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -3,9 +3,10 @@ import { type SchemaTypeDefinition } from 'sanity';
 import blockContent from './schemaTypes/blockContent';
 import event from './schemaTypes/event';
 import post from './schemaTypes/post';
-import revueScoute from './schemaTypes/revue-scoute';
+
+import revueScouteActuelle from './schemaTypes/revue-scoute-actuelle';
 import spectacle from './schemaTypes/spectacle';
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [post, blockContent, event, spectacle, revueScoute]
+  types: [post, blockContent, event, spectacle, revueScouteActuelle]
 };
