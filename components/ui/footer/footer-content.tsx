e13058a376f4c2c5f91251e38d1c7774acd92b3f
dobzacode <contact@corentinkittel.fr>
2024-08-12 14:54:40 +0200

    fix footer on mobile

--- components/ui/footer/footer-content.tsx
+++ components/ui/footer/footer-content.tsx
@@ -8,12 +8,12 @@ export default function FooterContent() {
         <Logo width={160} height={160} className="rounded-xs"></Logo>
         <p className="heading font-medium">ACTE 5</p>
       </div>
-      <nav className="flex flex-wrap justify-center gap-3xl max-laptop:w-full tablet:flex-nowrap tablet:justify-between laptop:justify-normal">
+      <nav className="flex grid-cols-2 flex-wrap justify-center gap-3xl max-laptop:w-full max-tablet:grid  max-mobile-large:flex tablet:flex-nowrap tablet:justify-between laptop:justify-normal">
         <div className="flex flex-col items-center gap-sm max-tablet:hidden laptop-large:hidden">
           <Logo width={84} height={84} className="rounded-xs"></Logo>
           <p className="sub-heading whitespace-nowrap text-md font-medium">ACTE 5</p>
         </div>
-        <section className="flex flex-col gap-sm ">
+        <section className="flex flex-col gap-sm  max-mobile-large:mr-auto  ">
           <Link
             scroll={false}
             className="sub-heading pb-sm text-md font-medium hover:opacity-hover"
@@ -57,7 +57,7 @@ export default function FooterContent() {
             Contact
           </Link>
         </section>
-        <section className="flex flex-col gap-sm">
+        <section className="flex flex-col gap-sm ">
           <Link
             scroll={false}
             className="sub-heading pb-sm text-md font-medium hover:opacity-hover"
@@ -95,7 +95,7 @@ export default function FooterContent() {
             Contact
           </Link>
         </section>
-        <section className="flex flex-col gap-sm">
+        <section className="flex flex-col gap-sm   max-mobile-large:w-full ">
           <p className="sub-heading pb-sm text-md font-medium ">Informations légales</p>
           <Link className="body hover:opacity-hover" href="politique-confidentialite">
             Politique de confidentialité
@@ -104,7 +104,7 @@ export default function FooterContent() {
             Mentions légales
           </Link>
         </section>
-        <section className="flex flex-col gap-sm">
+        <section className="flex flex-col gap-sm max-mobile-large:w-full">
           <p className="sub-heading pb-sm text-md font-medium ">Nous suivre</p>
           <a href="https://www.instagram.com/acte_5" className="body opacity-hover">
             Instagram
