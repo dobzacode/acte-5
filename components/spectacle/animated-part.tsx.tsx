df8ed7759640bf44096be524a9e66c2e2f0774db
dobzacode <contact@corentinkittel.fr>
2024-09-18 15:31:50 +0200

    fix animated-part

--- components/spectacle/animated-part.tsx.tsx
+++ components/spectacle/animated-part.tsx.tsx
@@ -45,98 +45,101 @@ export default function AnimatedPart({ isSpectacle }: { isSpectacle?: boolean })
         observerRef.current.unobserve(video);
       }
     };
-  }, [controlsButton]);
+  }, [controlsButton, controlsDiv]);
 
   return (
     <div className="-mr-2xl flex h-full w-fit items-center max-laptop:-mb-2xl max-tablet:-mb-4xl laptop:-mr-4xl laptop-large:-mr-8xl">
-      <motion.div
-        animate={controlsDiv}
-        initial="hidden"
-        variants={{
-          hidden: {
-            y: -700,
-            maxHeight: '120px !important',
-            opacity: 0
-          },
-          enter: {
-            y: 0,
-            opacity: 1,
-            transition: {
-              opacity: {
-                duration: 0.2
-              },
-              y: {
-                type: 'spring',
-                stiffness: 20
-              },
-              maxHeight: {
-                delay: 1.7,
-                duration: 0.35
-              }
-            },
-            maxHeight: '220px'
-          },
-          exit: {
-            y: -700
-          }
-        }}
-        className="card relative z-20 -mr-4xl flex h-fit w-fit shrink-0 flex-col items-center gap-md overflow-hidden max-laptop:-mt-2xl max-tablet:-mt-xl max-mobile-large:mt-md laptop:gap-xl"
-      >
-        {isSpectacle && (
-          <p className="sub-heading w-full max-w-[30ch] text-pretty text-center">
-            Avez-vous des questions sur nos événements ou souhaitez-vous réserver pour un groupe ?
-          </p>
-        )}
-        {!isSpectacle && (
-          <p className="sub-heading w-full max-w-[30ch] text-pretty text-center">
-            Besoin de communiquer ? <br />
-            Des questions ?
-          </p>
-        )}
+      <div className="-mr-4xl mt-5xl max-laptop:mt-sm max-tablet:-mt-2xl max-mobile-large:-mt-2xl laptop:-mr-0">
         <motion.div
-          animate={controlsButton}
+          key="wrapper-animated-div"
+          animate={controlsDiv}
           initial="hidden"
           variants={{
             hidden: {
-              x: 200,
-              y: 100
+              x: -700,
+              opacity: 0,
+              maxHeight: '120px'
             },
             enter: {
               x: 0,
-              y: 0,
+              opacity: 1,
               transition: {
-                type: 'spring',
-                stiffness: 50
-              }
+                opacity: {
+                  duration: 0.2
+                },
+                x: {
+                  type: 'spring',
+                  stiffness: 20
+                },
+                maxHeight: {
+                  delay: 1.7,
+                  duration: 0.35
+                }
+              },
+              maxHeight: '220px'
             },
             exit: {
-              x: 200,
-              y: 100
+              x: -700
             }
           }}
-          className="group flex w-full origin-center justify-center hover:opacity-90"
+          className="card relative z-20 flex h-fit max-h-0 w-fit shrink-0 flex-col items-center gap-md overflow-hidden laptop:gap-xl"
         >
-          <Link
-            href={
-              isSpectacle
-                ? '/spectacles-strasbourg/contact'
-                : '/agence-evenementielle-strasbourg/contact'
-            }
-            className="sub-heading shadow-primary-sm 0 mx-auto flex grow items-center gap-xs rounded-sm border-b-2 border-r-2 border-primary-600 bg-primary px-md py-sm text-center text-white laptop:gap-sm laptop:px-lg laptop:py-md"
-            scroll={false}
+          {isSpectacle && (
+            <p className="sub-heading w-full min-w-[14ch] max-w-[30ch] text-pretty text-center">
+              Avez-vous des questions sur nos événements ou souhaitez-vous réserver pour un groupe ?
+            </p>
+          )}
+          {!isSpectacle && (
+            <p className="sub-heading w-full min-w-[14ch] max-w-[30ch] text-pretty text-center">
+              Besoin de communiquer ? <br />
+              Des questions ?
+            </p>
+          )}
+          <motion.div
+            animate={controlsButton}
+            initial="hidden"
+            variants={{
+              hidden: {
+                x: 200,
+                y: 100
+              },
+              enter: {
+                x: 0,
+                y: 0,
+                transition: {
+                  type: 'spring',
+                  stiffness: 50
+                }
+              },
+              exit: {
+                x: 200,
+                y: 100
+              }
+            }}
+            className="group flex w-full origin-center justify-center hover:opacity-90"
           >
-            <span className="w-full text-center">
-              {isSpectacle ? 'Communiquons ensemble' : 'Contactez-nous'}
-            </span>
-          </Link>
+            <Link
+              href={
+                isSpectacle
+                  ? '/spectacles-strasbourg/contact'
+                  : '/agence-evenementielle-strasbourg/contact'
+              }
+              className="sub-heading shadow-primary-sm 0 mx-auto flex grow items-center gap-xs rounded-sm border-b-2 border-r-2 border-primary-600 bg-primary px-md py-sm text-center text-white laptop:gap-sm laptop:px-lg laptop:py-md"
+              scroll={false}
+            >
+              <span className="w-full text-center">
+                {isSpectacle ? 'Communiquons ensemble' : 'Contactez-nous'}
+              </span>
+            </Link>
+          </motion.div>
         </motion.div>
-      </motion.div>
-      <div className="relative h-[700px] w-[80vw] shrink-0 origin-top overflow-hidden max-laptop:scale-85 max-tablet:scale-80 max-mobile-large:pr-5xl tablet:w-[700px]">
+      </div>
+      <div className="relative h-[700px] w-[100vw] shrink-0 origin-top overflow-hidden max-laptop:scale-85 max-tablet:h-[650px] max-[560px]:scale-100 max-mobile-large:mb-3xl max-mobile-large:h-[450px] max-mobile-large:pr-5xl tablet:w-[700px]">
         <video
           ref={videoRef}
           muted
           playsInline
-          className="absolute top-0 h-full shrink-0 object-none object-center"
+          className="object-fit absolute top-0 -mt-lg h-[1080px] shrink-0 object-top"
         >
           <source src={'/assets/logo-animation.webm'} type="video/webm" />
           Votre navigateur ne supporte pas la balise video.
